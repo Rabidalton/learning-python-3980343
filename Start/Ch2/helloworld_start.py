088ee424ad5babcc53887d1c8f6df6341f39bd05
John Pittman Gibson <117667506+Rabidalton@users.noreply.github.com>
2025-07-14 22:57:22 +0000

    new changes

--- Start/Ch2/helloworld_start.py
+++ Start/Ch2/helloworld_start.py
@@ -1,2 +1,11 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for HelloWorld
+print("Hello World!")
+name = input("What is your name? ")
+print("Nice to meet you", name)
+
+CollegeName = input("What is the name of your college? ")
+print ("Good luck at", CollegeName,".")
+
+Age = input("How old are you? ")
+print("With this age, ", Age, "years old, I can tell that you are very young. ")
--- Start/Ch2/variables_start.py
+++ Start/Ch2/variables_start.py
@@ -10,12 +10,20 @@ mystr = "This is a string"
 mybool = True
 
 # We can display the content of a variable using the print() function
+# print(myint)
+# print(myfloat)
+# print(mystr)
+# print(mybool)
 
 
 # Operators are used to perform operations on variables
-
-
+print(myint + myfloat)
+print(mybool >= myfloat)
+print(myfloat == 10)
+print(myint < 24)
 # Logical and comparison operators 
-
+print(myfloat < 15 or myint >= 20)
 
 # re-declaring a variable works
+myint = 30
+print(myint)
