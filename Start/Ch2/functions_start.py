315d3112626e489608eb8417cda14ef3c999f078
John Pittman Gibson <117667506+Rabidalton@users.noreply.github.com>
2025-07-16 11:01:15 +0000

    added function

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -3,41 +3,41 @@
 
 
 # define a basic function
-# print("hello world!")
-# name = input("What is your name? ")
-# print("Nice to meet you,", name)
+print("hello world!")
+name = input("What is your name? ")
+print("Nice to meet you,", name)
 
 # function that takes parameters
-# def conversation():
-#   print("hello world!")
-#   name = input("What is your name? ")
-#   print("Nice to meet you,", name)
+def conversation():
+  print("hello world!")
+  name = input("What is your name? ")
+  print("Nice to meet you,", name)
 
-# conversation()
+conversation()
 
-# def dialog():
-#   print("Hello. Nice to meet you")
-#   name = input("What's your name? ")
-#   state = input("How are you?")
-#   if(state == "I am fine"):
-#     print("Good to know, ", name)
-#   elif state == "I am not well":
-#     print("I am so sorry, ", name,".")
-#   else: 
-#     print("Ok. Thanks for letting me know.")
+def dialog():
+  print("Hello. Nice to meet you")
+  name = input("What's your name? ")
+  state = input("How are you?")
+  if(state == "I am fine"):
+    print("Good to know, ", name)
+  elif state == "I am not well":
+    print("I am so sorry, ", name,".")
+  else: 
+    print("Ok. Thanks for letting me know.")
 
-# dialog()
+dialog()
 
 # function that returns a value
-# def multiplier(n):
-#     return n*n
-# val = multiplier(8)
-# print(val)
+def multiplier(n):
+    return n*n
+val = multiplier(8)
+print(val)
 
 # function with default value for an parameter
 def currency_converter(user_input):
     rate = int(input("What is conversion rate? "))
-    if (user_input > 0 and rate !=0):
+    if (user_input > 0 or rate !=0):
         return user_input/rate
     else:
         print("Undefined")
@@ -49,3 +49,9 @@ ans = currency_converter(amount_to_convert)
 print(f"{amount_to_convert} in {FromCurrency} is {ans} in {ToCurrency}.")
 
 # function with variable number of parameters
+def age_adder(*ages):
+    result = 0
+    for n in ages:
+        result = result + n
+    return result
+print(age_adder(30, 40, 25, 18, 19))
